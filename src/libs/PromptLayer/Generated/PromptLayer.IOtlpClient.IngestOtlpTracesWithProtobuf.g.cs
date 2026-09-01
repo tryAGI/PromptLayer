35253494f9e52701c462add69bc4964c28c74bda
Konstantin S. <havendv@gmail.com>
2026-09-01 22:21:44 +0400

    feat: Updated OpenAPI spec (#72)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/PromptLayer/Generated/PromptLayer.IOtlpClient.IngestOtlpTracesWithProtobuf.g.cs
@@ -0,0 +1,48 @@
+#nullable enable
+
+namespace PromptLayer
+{
+    public partial interface IOtlpClient
+    {
+        /// <summary>
+        /// Ingest Traces (OTLP)
+        /// </summary>
+        /// <param name="contentType"></param>
+        /// <param name="contentEncoding"></param>
+        /// <param name="closeAfter">
+        /// Default Value: false
+        /// </param>
+        /// <param name="request"></param>
+        /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
+        /// <param name="cancellationToken">The token to cancel the operation with</param>
+        /// <exception cref="global::PromptLayer.ApiException"></exception>
+        global::System.Threading.Tasks.Task<global::PromptLayer.OtlpExportTraceServiceResponse> IngestOtlpTracesWithProtobufAsync(
+            global::PromptLayer.IngestOtlpTracesContentType contentType,
+
+            byte[] request,
+            global::PromptLayer.IngestOtlpTracesContentEncoding? contentEncoding = default,
+            bool? closeAfter = default,
+            global::PromptLayer.AutoSDKRequestOptions? requestOptions = default,
+            global::System.Threading.CancellationToken cancellationToken = default);
+        /// <summary>
+        /// Ingest Traces (OTLP)
+        /// </summary>
+        /// <param name="contentType"></param>
+        /// <param name="contentEncoding"></param>
+        /// <param name="closeAfter">
+        /// Default Value: false
+        /// </param>
+        /// <param name="request"></param>
+        /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
+        /// <param name="cancellationToken">The token to cancel the operation with</param>
+        /// <exception cref="global::PromptLayer.ApiException"></exception>
+        global::System.Threading.Tasks.Task<global::PromptLayer.AutoSDKHttpResponse<global::PromptLayer.OtlpExportTraceServiceResponse>> IngestOtlpTracesWithProtobufAsResponseAsync(
+            global::PromptLayer.IngestOtlpTracesContentType contentType,
+
+            byte[] request,
+            global::PromptLayer.IngestOtlpTracesContentEncoding? contentEncoding = default,
+            bool? closeAfter = default,
+            global::PromptLayer.AutoSDKRequestOptions? requestOptions = default,
+            global::System.Threading.CancellationToken cancellationToken = default);
+    }
+}
